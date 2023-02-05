c8f846c7971741065dbaa16298d9c59c7e530c7e
fanoway <86997883+fanoway@users.noreply.github.com>
2023-02-05 14:29:35 -0800

    change inputs to borrwed references

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,18 @@
 /// If 'include_end' = false, then the number of steps will remain the same,
 /// but the final value will be one increment short of end.
 /// This is useful behaviour if a periodic linspace is desired.
-pub fn linspace(start: f64, end: f64, n_steps: i32, include_end: bool) -> Vec<f64> {
+pub fn linspace(start: &f64, end: &f64, n_steps: &i32, include_end: &bool) -> Vec<f64> {
     // Get step size based on 'include_end'
     let interval: f64 = end - start;
 
-    let step_size = if include_end {
-        interval / (n_steps - 1) as f64
+    let step_size = if *include_end {
+        interval / (*n_steps as f64 - 1.0)
     } else {
-        interval / n_steps as f64
+        interval / (*n_steps as f64)
     };
 
     let mut line: Vec<f64> = Vec::new();
-    for n in 0..n_steps {
+    for n in 0..*n_steps {
         line.push(start + step_size * n as f64);
     }
 
@@ -27,7 +27,7 @@ mod tests {
 
     #[test]
     fn test_linspace_include_end() {
-        let result = linspace(1.0, 10.0, 10, true);
+        let result = linspace(&1.0, &10.0, &10, &true);
         assert_eq!(
             result,
             vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
@@ -36,7 +36,7 @@ mod tests {
 
     #[test]
     fn test_linspace_exclude_end() {
-        let result = linspace(1.0, 11.0, 10, false);
+        let result = linspace(&1.0, &11.0, &10, &false);
         assert_eq!(
             result,
             vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
